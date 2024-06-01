b570ff009110a0b69402e3b7e3935cd191f77a71
Veeci <doanvietquang15102003@gmail.com>
2024-06-01 11:04:50 +0700

    completed home, meal detail function

---
+++ app/src/main/java/com/example/mealreceiptapp/Meal2Activity.java
@@ -0,0 +1,17 @@
+package com.example.mealreceiptapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Meal2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
